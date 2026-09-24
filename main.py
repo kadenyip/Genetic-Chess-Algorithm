c451e2c246184cc7757e26d3b1e4375e2a96034c
Kaden Yip <164582403+kadenyip@users.noreply.github.com>
2026-09-23 21:05:51 -0700

    Implement main game loop for chess application

---
+++ main.py
@@ -0,0 +1,40 @@
+import chess
+
+from logic import (
+    save_board_svg,
+    make_move,
+    get_legal_moves,
+    is_checkmate,
+    computer_move,
+    get_current_evaluation
+)
+
+def main():
+    print("Type quit or exit to leave the game")
+    while not is_checkmate():
+        try:
+            user_move = input("Make your move: ")
+            if user_move.lower() in ["quit", "exit"]:
+                print("Thanks for playing!")
+                break
+
+            if make_move(user_move) == False:
+                continue
+
+            score = get_current_evaluation()
+            print(f"Current Evaluation: {score}\n")
+
+            if is_checkmate():
+                print("Checkmate")
+                break
+        
+            computer_move()
+            score = get_current_evaluation()
+            print(f"Current Evaluation: {score}\n")
+
+        except KeyboardInterrupt:
+            print("Game forcibly aborted by user input")
+            break
+
+if __name__ == "__main__":
+    main()
